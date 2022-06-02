bd0ea1e62684e0ee048b3f5bf9ba4786a765d652
Nadia ben kassem <90767449+nadiasGame@users.noreply.github.com>
2022-06-02 10:50:35 +0200

    nytt

--- js/index.js
+++ js/index.js
@@ -1,222 +1,77 @@
-const notificationsButton = document.querySelector('#grant-button');
-const showNotificationButton = document.querySelector('#notifications-button');
-const pictureElem = document.querySelector('#gallery');
-const cameraButton = document.querySelector('#start-camera');
-const videoElem = document.querySelector('#camera');
-const takePictureButton = document.querySelector('#take-picture');
+const cameraButton = document.querySelector('.button-div-camera');
+const videoElem  = document.querySelector('#camera');
+const takePictureButton = document.querySelector('.button-div');
+const galleryElem = document.querySelector('#gallery')
+const streamElem = document.querySelector('.stream-window');
+const pictureElem = document.querySelector('.picture-window')
+const newPicturesElem = document.querySelector('.button-div-pic');
+
+
 const canvas = document.querySelector('#picture');
-const galleryElem = document.querySelector('#gallery');
 const ctx = canvas.getContext('2d');
+canvas.width = 342;
+canvas.height = 310;
+
+
 let stream;
-const images = [];
+let images;
 
-let picturesFromStorage = JSON.parse(localStorage.getItem('myPictures'));
-let imagesFromStorage = JSON.parse(localStorage.getItem('gallery'));
-let notificationPermission = '';
-
-cameraButton.addEventListener('click', async () => {
-    if ('mediaDevices' in navigator) {
-        stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-        console.log(stream);
-        videoElem.srcObject = stream;
-    }
-});
-
-/*function createNotification() {
-    const text = 'Detta är en notifikation!';
-
-    const notification = new Notification('Notis', { body: text });
-
-    notification.addEventListener('click', () => {
-        window.open('https://localhost:4000');
-    });
+let imagesStored = JSON.parse(localStorage.getItem('wedding-photos-App'))
+if (imagesStored) {
+    images = imagesStored
+} else {
+    images = [];
 }
 
-function requestNotificationsPermission() {
-    Notification.requestPermission().then((permission) => {
-        console.log(permission);
-        notificationPermission = permission;
-    });
-}
-
-notificationsButton.addEventListener('click', () => {
-    requestNotificationsPermission();
-});
-
-showNotificationButton.addEventListener('click', () => {
-    if (notificationPermission === 'granted') {
-        createNotification();
+/* stream.html // Autostreamar kameran när behörighet ges*/
+async function cameraAutoStart(){
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) { // browser support
+            stream = await navigator.mediaDevices.getUserMedia({video:true, audio:false});
+            videoElem.srcObject = stream;
+            videoElem.load();
+            videoElem.play();
     }
-});*/
+};
 
 
-
-
-/*function createPictureItem(picture) {
-    const pictureElem = document.createElement('li');
-    pictureElem.innerHTML = picture.task;
-    pictureElem.append(pictureElem);
-
-  pictureElem.addEventListener('click', () => {
-        picturesFromStorage = JSON.parse(localStorage.getItem('myPictures'));
-        /*
-        1. Ta bort vald bild från arrayen (använd filter() eller en splice())
-        2. Spara den nya arrayen till localStorage
-       */ 
-       /* console.log(picture);
-   pictureElem.remove();
-
-      picturesFromStorage = picturesFromStorage.filter((pictureItem) => {
-            console.log(pictureItem);
-            if (pictureItem.task !== picture.task) {
-                return pictureItem;
-            }
-        });
-
-        saveToLocalStorage(picturesFromStorage);
-
-       /** En variant med splice istället */
-
-       // Vi behöver först hitta indexet vår todo ligger på i arrayen
-    //    const index = picturesFromStorage.findIndex((pictureItem) => pictureItem.task === picture.task);
-    //    picturesFromStorage.splice(index, 1);
-    //    saveToLocalStorage(picturesFromStorage);
-
-    
-    takePictureButton.addEventListener('click', () => {
-        ctx.drawImage(videoElem, 0, 0, canvas.width, canvas.height);
-        const imageData = canvas.toDataURL('image/png'); // Konverterar det till en png-bild
-    
-        images.push({
-            id: images.length,
-            image: imageData        
-        });
-    
-        localStorage.setItem('cameraApp', JSON.stringify(images));
-    });
-
-function displayPictures(pictures) {
-    for(const picture of pictures) {
-        console.log(picture);
-        createPictureItem(picture);
-    }
-}
-
-function saveToLocalStorage(pictures) {
-    localStorage.setItem('myPictures', JSON.stringify(pictures));
-}
-
-/*async function getPictures() {
-    const response = await fetch('https://awesome-todo-api.herokuapp.com/tasks');
-    const data = await response.json();
-
-    console.log(data);
-    displayPictures(data.pictures);
-    saveToLocalStorage(data.pictures);
-}
-
-function requestNotificationPermission() {
-    Notification.requestPermission().then((permission) => {
-        notificationPermission = permission;
-
-        loadPictures();
-    });
-}
-
-function createNotification(text) {
-    console.log(notificationPermission);
-    if (notificationPermission === 'granted') {
-        const icon = 'icons/icon-192.png';
-
-        const notification = new Notification('Awesome Picture App', { body: text, icon: icon });
-
-        notification.addEventListener('click', () => {
-            getPictures();
-        });
-    }
-}*/
-
-/*function loadPictures() {
-    if (picturesFromStorage && picturesFromStorage.length > 0) {
-        createNotification('Dina pictures kommer från localstorage och kan vara gamla. Vill du göra en uppdatering?');
-        displayPictures(picturesFromStorage);
-    } else {
-        createNotification('Du har de senaste!');
-        getPictures();
-    }
-}*/
-function createImage(image) {
-    const imageElem = document.createElement('img');
-    imageElem.setAttribute('src', image.image);
-
-    galleryElem.append(imageElem);
-}
-
-function getImages() {
-    const images = JSON.parse(localStorage.getItem('cameraApp'));
-
-    for(const image of images) {
-        createImage(image);
-    }
-}
-
-getImages();
-
-window.addEventListener('load', async () => {
-    if('serviceWorker' in navigator){
-        try {
-            await navigator.serviceWorker.register('service-worker.js');
-        } catch(err) {
-            console.error('Whooopsie!', err)
-        }
-    }
-});
-
-//requestNotificationPermission();
-
-cameraButton.addEventListener('click', async () => {
-    if ('mediaDevices' in navigator) {
-        stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
-        console.log(stream);
-        videoElem.srcObject = stream;
-    }
-});
-
-takePictureButton.addEventListener('click', () => {
+takePictureButton.addEventListener ('click', () => {
     ctx.drawImage(videoElem, 0, 0, canvas.width, canvas.height);
-    const imageData = canvas.toDataURL('image/png'); // Konverterar det till en png-bild
-
+    const imageData = canvas.toDataURL('image/png');
+    takePictureButton.style.display='none'
+    streamElem.style.display='none' 
+    pictureElem.style.display='block' 
     images.push({
-        id: images.length,
-        image: imageData        
+        id: images.lenght,
+        image: imageData
     });
-
-    localStorage.setItem('cameraApp', JSON.stringify(images));
+    localStorage.setItem('wedding-photos-App', JSON.stringify(images));
 });
 
-function createImage(image) {
+newPicturesElem.addEventListener ('click', () => {
+    streamElem.style.display='block' 
+    pictureElem.style.display='none' 
+    takePictureButton.style.display = 'block'
+});
+
+
+function newFunction() {
+    cameraAutoStart();
+}
+
+function createImage(image){
     const imageElem = document.createElement('img');
     imageElem.setAttribute('src', image.image);
-console.log(image)
+
     galleryElem.append(imageElem);
-}
 
-function getImages() {
-    const images = JSON.parse(localStorage.getItem('cameraApp'));
+};
 
-    for(const image of images) {
+function getImages(){
+    for (let image of images){
         createImage(image);
     }
-}
 
+};
+
+cameraAutoStart();
 getImages();
-
-galleryElem.addEventListener('click', () => {
-    /*
-    1. Ta bort vald todo från arrayen (använd filter() eller en splice())
-    2. Spara den nya arrayen till localStorage
-   */ 
-    console.log(gallery);
-    galleryElem.remove();
-});
-
--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,31 @@
-    self.addEventListener('install', (event) => {
-        event.waitUntil(
-            caches.open('v1').then((cache) => {
-                return cache.addAll(['offline.html', 'styles.css']);
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('v1').then((cache) => {
+            return cache.addAll(['index.html', 'js/index.js', 'offline.html', 'favicon.ico', 'css/style-offline.css']);
+        })
+    );
+    self.skipWaiting();
+    console.log('Service worker installed!');
+});
+
+self.addEventListener('activate', (event) => {
+    self.skipWaiting();
+    console.log('Service worker activated!');
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log(event.request.url); // Skriv ut url:en på varje nätverksförfrågan
+    if (navigator.onLine) {
+        console.log('Du är online!');
+    } else {
+        console.log('Du är offline!');
+        console.log('Begärd url:', event.request);
+        event.respondWith(
+            caches.match(event.request).then((response) => {
+                console.log('response', response);
+                if (response) return response;
+                else return caches.match(new Request('offline.html'));
             })
-        )
-    
-        self.skipWaiting();
-        console.log('Installed service worker at ', new Date().toLocaleTimeString());
-    });
-    
-    self.addEventListener('activate', (event) => {
-        self.skipWaiting();
-        console.log('Activated service worker at ', new Date().toLocaleTimeString());
-    });
-    
-    self.addEventListener('fetch', async (event) => {
-        console.log(event.request.url);
-        if (!navigator.onLine) { // Kolla om vi har internet eller ej, ger tillbaka true/false
-            console.log('Offline');
-            event.respondWith(
-                caches.match(event.request).then((response) => {
-                    console.log('RESPONSE:', response);
-                    if (response) {
-                        return response;
-                    } else {
-                        return caches.match(new Request('offline.html'));
-                    }
-                })
-            );
-        } else {
-            console.log('Online');
-            const response = await updateCache(event.request);
-            return response;
-        }
-    });
-    
-    async function updateCache(request) {
-        const response = await fetch(request);
-        const cache = await caches.open('v1');
-    
-        cache.put(request, response.clone());
-    
-        return response;
-    };
+        );
+    }
+});
